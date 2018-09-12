6bfeeaed43910dc3cc4a125c5067b8d3aa46e18f
Kieve Chua <kievechua@gmail.com>
2018-09-12 18:14:51 +0800

    add base calculation

--- src/lib/voucher.js
+++ src/lib/voucher.js
@@ -9,14 +9,18 @@ export function calculateDueDate (voucher) {
 export function calculateVoucherItem (voucherItem) {
   let rate = math.multiply(voucherItem.exchangeRate, voucherItem.baseRate)
   let subTotal = math.multiply(voucherItem.quantity, rate)
+  let baseSubTotal = math.multiply(voucherItem.quantity, voucherItem.baseRate)
   let taxTotal = math.chain(subTotal).multiply(voucherItem.taxPercentage).divide(100).done()
+  let baseTaxTotal = math.chain(baseSubTotal).multiply(voucherItem.taxPercentage).divide(100).done()
   let total = math.add(subTotal, taxTotal)
 
   return {
     ...voucherItem,
     rate,
     subTotal,
+    baseSubTotal,
     taxTotal,
+    baseTaxTotal,
     total
   }
 }
@@ -28,12 +32,14 @@ export function calculateVoucher (voucher) {
   newVoucher.voucherItems = voucherItems.map(calculateVoucherItem)
 
   const validVoucherItems = newVoucher.voucherItems.filter(vi => !vi.isDeleted)
-  
+
   newVoucher.subTotal = validVoucherItems.reduce((sum, voucherItem) => math.sum(sum, voucherItem.subTotal), 0)
+  newVoucher.baseSubTotal = validVoucherItems.reduce((sum, voucherItem) => math.sum(sum, voucherItem.baseSubTotal), 0)
   newVoucher.taxTotal = validVoucherItems.reduce((sum, voucherItem) => math.sum(sum, voucherItem.taxTotal), 0)
+  newVoucher.baseTaxTotal = validVoucherItems.reduce((sum, voucherItem) => math.sum(sum, voucherItem.baseTaxTotal), 0)
   newVoucher.total = math.sum(newVoucher.subTotal, newVoucher.taxTotal)
   newVoucher.balance = math.subtract(newVoucher.total, 0)
-  
+
   if (newVoucher.issueDate) {
     newVoucher.issueDate = startOfDay(newVoucher.issueDate)
   }
@@ -62,7 +68,7 @@ export function calculateVoucherWithBalance (params) {
   voucher.issueDate = startOfDay(voucher.issueDate)
   voucher.voucherDate = startOfDay(voucher.voucherDate)
   voucher.dueDate = calculateDueDate(voucher)
-  
+
   const validPayments = payments.filter(vi => vi.status === 'PAID')
   const totalVoucherPayment = validPayments.reduce((sum, vp) => math.sum(sum, vp.amount), 0)
   voucher.balance = math.subtract(voucher.total, totalVoucherPayment)
@@ -80,7 +86,7 @@ export function sortVoucherItems (voucherItems) {
 
     // Current issue is sorted by number.
     return (vi.isDeleted ? 10000000 : 0) +
-      (parseInt(sequence) * 1000) + 
+      (parseInt(sequence) * 1000) +
       parseInt(code)
   })
 }
